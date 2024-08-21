6c072840a13e03abf1e8d4a84cf31cd83b79a718
timofeev-tema <timov000@mail.ru>
2024-08-21 23:39:59 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict ={ 'Artem': 2000 , 'Olya': 2002, 'kleo':2022}
+print(my_dict)
+print(my_dict['kleo'])
+my_dict ['Timur']=1993
+print(my_dict)
+my_dict.update({'Dima':2004,
+                'Alena':2009})
+print(my_dict)
+del my_dict['kleo']
+print(my_dict)
+My_set= {1,2,3,4,5,1,2,3,4,"W","W"}
+My_set.update({6,7})
+print(My_set)
+print(My_set.discard(1))
+print(My_set)
